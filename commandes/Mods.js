4fec3ead02be33a0f94563b75db4cb149313ef1d
Jashon254 <158827004+Jashon254@users.noreply.github.com>
2024-10-18 02:11:37 +0300

    Update Mods.js

--- commandes/Mods.js
+++ commandes/Mods.js
@@ -70,7 +70,7 @@ const sleep =  (ms) =>{
   
           const sticker = new Sticker(buffer.data, {
             pack: nomAuteurMessage,
-            author: "Anyway-md",
+            author: "DrJay-md",
             type: StickerTypes.FULL,
             categories: ['🤩', '🎉'],
             id: '12345',
